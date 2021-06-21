156b9acb8a840bceb963937e41044328eb961ec5
김재성 (Kim, Jaesung) <15705385+sorrykim@users.noreply.github.com>
2021-06-21 23:44:54 +0900

    implement fallback

--- background.js
+++ background.js
@@ -8,7 +8,6 @@ const SPOTIFY_URL_REGEX = /^https?:\/\/(?:[^./?#]+\.)?spotify\.com/;
 // When the browser-action button is clicked...
 browser.browserAction.onClicked.addListener(function (tab) {
   // TODO: support any open spotify tab, not just active tab right now.
-  console.log('browser action')
   if (isSpotifyWebPlayer(tab.url)) {
     browser.tabs.sendMessage(tab.id, { text: 'request_nowplaying_track_info' }, openGeniusLyric);
   }
@@ -26,7 +25,21 @@ function getGeniusUrl(name, artists) {
   return `https://genius.com/${pathname}`;
 }
 
+function getGeniusSearchUrl(name, artists) {
+  // FIXME: improve santizing logic, or search in genius.com as a fallback.
+  const [artist] = artists.split(', ');
+  const [title] = name.split(' (feat.');
+  return encodeURI(`https://genius.com/search?q=${artist} ${title}`);
+}
+
 // A function to use as callback
-function openGeniusLyric({name, artists}) {
-  browser.tabs.create({ url: getGeniusUrl(name, artists) });
+async function openGeniusLyric({name, artists}) {
+  const geniusUrl = getGeniusUrl(name, artists);
+  const res = await fetch(geniusUrl);
+  if (res.ok) {
+    await browser.tabs.create({ url: geniusUrl });
+  } else {
+    const searchUrl = getGeniusSearchUrl(name, artists);
+    await browser.tabs.create({ url: searchUrl });
+  }
 }
